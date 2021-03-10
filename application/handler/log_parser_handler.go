f6fcfa3d242c0d52792fe23226f5a5f3c7b3ab49
Regis Zanandrea <regis.zanandrea@endurance.com>
2021-03-10 19:07:07 -0300

    LOG-PARSER add handler

---
+++ application/handler/log_parser_handler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"api-gateway-log-parser/pkg/apigateway"
+	"context"
+	"errors"
+	"os"
+)
+
+type LogParserHandler struct {
+	service apigateway.ApiGatewayLogService
+}
+
+func NewLogParserHandler(service apigateway.ApiGatewayLogService) *LogParserHandler {
+	return &LogParserHandler{service: service}
+}
+
+func (h *LogParserHandler) HandleApiGatewayLogParser(ctx context.Context) error {
+	if len(os.Args) < 2 {
+		return errors.New("path parameter not provided")
+	}
+
+	path := os.Args[1]
+
+	if path == "" {
+		return errors.New("path parameter could not be empty")
+	}
+
+	return h.service.Parse(path)
+}

5eddddb4ce8a60370009532092a5d0efb02a38e2
Regis Zanandrea <regis.zanandrea@endurance.com>
2021-03-11 10:25:26 -0300

    EXPORT-BY implement ExportByService

--- application/service/apigateway.go
+++ application/service/apigateway.go
@@ -4,9 +4,13 @@ import (
 	"api-gateway-log-parser/pkg/apigateway"
 	"api-gateway-log-parser/pkg/apigateway/repository"
 	"api-gateway-log-parser/pkg/filesystem"
+	"bytes"
+	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 
 type ApiGatewayLogService struct {
@@ -77,6 +81,51 @@ func (a *ApiGatewayLogService) Parse(path string) error {
 	return nil
 }
 
+func (a *ApiGatewayLogService) ExportByService(service string) error {
+	limit := 200
+
+	columns := apigateway.GetJsonFieldsFromLogStruct()
+
+	var buffer bytes.Buffer
+
+	w := csv.NewWriter(&buffer)
+	defer w.Flush()
+
+	separator := ';'
+	w.Comma = separator
+	err := w.Write(columns)
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		logs, err := a.repo.GetByService(service, limit)
+
+		if err != nil {
+			return err
+		}
+
+		if logs == nil {
+			break
+		}
+
+		values := getValuesFromLogs(logs)
+
+		err = w.WriteAll(values)
+
+		if err != nil {
+			return err
+		}
+
+		fileName := fmt.Sprintf("service-%s-%s.csv", service, time.Now().Format("02-01-2006-15-04-05"))
+
+		a.filesystem.Write(fileName, buffer.String())
+	}
+
+	return nil
+}
+
 func (a *ApiGatewayLogService) addLogs(logs []*apigateway.Log, wg *sync.WaitGroup) {
 	err := a.repo.Add(logs...)
 	if err != nil {
@@ -84,4 +133,14 @@ func (a *ApiGatewayLogService) addLogs(logs []*apigateway.Log, wg *sync.WaitGrou
 	}
 
 	defer wg.Done()
+}
+
+func getValuesFromLogs(logs []*apigateway.Log) [][]string {
+	var values [][]string
+	for _, l := range logs {
+		r := l.ToSlice()
+
+		values = append(values, r)
+	}
+	return values
 }

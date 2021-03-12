3c1a1d5002e3f3dfdaa78caf31c42dd51e50f612
Regis Zanandrea <regis.zanandrea@endurance.com>
2021-03-12 15:57:59 -0300

    TESTS improve error messages

--- application/handler/log_parser_handler.go
+++ application/handler/log_parser_handler.go
@@ -11,19 +11,24 @@ type LogParserHandler struct {
 	service apigateway.LogService
 }
 
+var (
+	ErrPathParameterNotFound        = errors.New("path parameter not provided")
+	ErrPathParameterCouldNotBeEmpty = errors.New("path parameter could not be empty")
+)
+
 func NewLogParserHandler(service apigateway.LogService) *LogParserHandler {
 	return &LogParserHandler{service: service}
 }
 
 func (h *LogParserHandler) HandleApiGatewayLogParser(ctx context.Context) error {
 	if len(os.Args) < 2 {
-		return errors.New("path parameter not provided")
+		return ErrPathParameterNotFound
 	}
 
 	path := os.Args[1]
 
 	if path == "" {
-		return errors.New("path parameter could not be empty")
+		return ErrPathParameterCouldNotBeEmpty
 	}
 
 	return h.service.Parse(path)
